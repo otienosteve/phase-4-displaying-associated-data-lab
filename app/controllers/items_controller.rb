9ffad2ab08e7313ca32975d56bc59910c06163b7
otienosteve <steveotieno701@gmail.com>
2022-09-12 21:04:38 +0300

    Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+def index
+    user_data=Item.all
+    render json: user_data, include: :user
+    
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def show
+    user_data=User.find_by(id:params[:id])
+    render json: user_data, include: :items
+    
+    end
+
 end
